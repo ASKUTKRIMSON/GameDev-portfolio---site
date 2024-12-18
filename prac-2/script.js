f76b91ed19ad3bea2183d3f084ba90ca4646cb32
ASKUT <askutkrimson@gmail.com>
2024-12-18 17:46:58 +0300

    Start

---
+++ prac-2/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let backToTopBtn = document.createElement('button');
+    backToTopBtn.innerText = 'Наверх';
+    backToTopBtn.style.position = 'fixed';
+    backToTopBtn.style.bottom = '20px';
+    backToTopBtn.style.right = '20px';
+    document.body.appendChild(backToTopBtn);
+
+    backToTopBtn.addEventListener('click', function() {
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
+});
